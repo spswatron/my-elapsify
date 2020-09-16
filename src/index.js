22188e26e48d80b7cd47feefce46135f5a03e5a6
Ashley Chang <ashley_e_chang@brown.edu>
2020-09-15 23:32:06 -0400

    work

--- src/index.js
+++ src/index.js
@@ -173,7 +173,7 @@ class ToDo extends React.Component {
         const newDate = new Date()
         if(this.state.checked && !(this.state.curTime.getDay() === this.state.date)) {
             const contentList = ['100 push-ups', '100 lunges', '1.2 mile run',
-            'shower', 'read Bible', 'journal', 'eat Chia', 'make Chia']
+            'shower', 'read Bible', 'journal', 'eat Chia', 'make Chia', '69 minutes research']
             const newList = this.state.entries.concat(contentList)
             const newDates = this.state.startTimes.concat(new Array(8).fill(newDate))
             const newActives = activeLengthen(this.state.activeStarts, contentList)
